68b2f5caa3140ef2e75a41479cf0974c2e56fe4c
Jakub Melnik <jakubmelnik@gmail.com>
2025-12-17 15:04:38 +0100

    Create LanguageData.lua
    The moduleScript with all the data

---
+++ LanguageData.lua
@@ -0,0 +1,16 @@
+local languageData = {
+	["en"] = {
+		["warncommandsetup"] = "%s is not set up properly, check TabletScript and add functionality to the command.",
+		["langerror"] = "LANGERROR: Key '%s' missing.",
+		["datastoregeterror"] = "DataStoreModule: Failed to get data for %s (Attempt %d/%d)",
+		["datastoremaxretries"] = "DataStoreModule: All %d attempts failed to get data for %s."
+	},
+	["pl"] = {
+		["warncommandsetup"] = "%s nie jest ustawiona, sprawdź TabletScript i dodaj funkcjonalność komendy.",
+		["langerror"] = "LANGERROR: Brakuje klucza '%s'.",
+		["datastoregeterror"] = "DataStoreModule: Nie udało się wczytać danych dla %s (Próba %d/%d)",
+		["datastoremaxretries"] = "DataStoreModule: Wszystkie %d prób zawiodło w wczytywaniu danych dla %s."
+	}
+}
+
+return languageData
